f1df15b6078ef030654418a6152ee2abaf62ae4f
helena224 <viherhelena@gmail.com>
2021-08-30 20:58:41 +0200

    add validation to text input
    that checks if text field is empty before adding new item to the list

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const list = document.getElementById("list-items");
+const inputText = document.getElementById("add-input");
+
 function addItem() {
-    const list = document.getElementById("list-items");
-    const inputText = document.getElementById("add-input");
     let newItem = document.createElement("li");
-    // create new element
+    if (inputText.value === "") return;
     newItem.innerHTML = `<input type='checkbox'><span class='item'>${inputText.value}</span>`;
     list.appendChild(newItem);
     inputText.value = "";
