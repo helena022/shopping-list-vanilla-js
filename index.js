904630a1dedb49fd099f4e9fea5450dd22631bb1
helena224 <viherhelena@gmail.com>
2021-08-30 22:16:00 +0200

    add functionality to remove item from list

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const list = document.getElementById("list-items");
-const inputText = document.getElementById("add-input");
-
 function addItem() {
+    const list = document.getElementById("list-items");
+    const inputText = document.getElementById("add-input");
+    
     let newItem = document.createElement("li");
     if (inputText.value === "") return;
-    newItem.innerHTML = `<input type='checkbox'><span class='item'>${inputText.value}</span>`;
+    newItem.innerHTML = `<input type="checkbox"><span class="item">${inputText.value}</span><button class="btn-remove" onClick="return this.parentNode.remove();">X</button>`;
     list.appendChild(newItem);
     inputText.value = "";
 }
